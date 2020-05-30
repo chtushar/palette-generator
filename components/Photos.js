ce55d175e95b0381b0c28d544f04eff1294e5f88
Tushar Choudhari <choudharit23@gmail.com>
2020-05-30 16:15:57 +0530

    taking images as props

--- components/Photos.js
+++ components/Photos.js
@@ -1,20 +1,11 @@
-function Photos() {
-  let links = [];
-  let random = Math.round(100 * Math.random());
-  for (let i = 0; i < 10; i++) {
-    links.push({
-      link: `https://i.picsum.photos/id/${300 + random + i}/400/225.jpg`,
-      id: 150 + 1 + i,
-    });
-  }
+import { useEffect, useState } from "react";
 
+const Photos = ({ links }) => {
   return (
     <div>
-      {links.map((el) => (
-        <img src={el.link} key={el.id}></img>
-      ))}
+      {links.map((el) => <img src={el.link} key={el.id}></img> || undefined)}
     </div>
   );
-}
+};
 
 export default Photos;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Head from "next/head";
 import Photos from "../components/Photos";
+import { v4 as uuid } from "uuid";
 
-export default function Home() {
+export default function Home({ links }) {
   return (
     <div className="container">
       <div className="row">
-        <Photos />
+        <Photos links={links} />
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  let links = [];
+  for (let i = 0; i < 10; i++) {
+    let ran = Math.round(200 * Math.random() + i);
+    links.push({
+      link: `https://i.picsum.photos/id/${ran}/400/225.jpg`,
+      id: uuid(),
+    });
+  }
+
+  return {
+    props: {
+      links,
+    },
+  };
 }
