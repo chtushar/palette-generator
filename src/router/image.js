282ed5c44dca6d2c6408f27834e17997a7e7f1cc
Tushar Choudhari <choudharit23@gmail.com>
2020-07-30 21:21:44 +0530

    done with backend

--- src/router/image.js
+++ src/router/image.js
@@ -1,24 +1,25 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const router = new express.Router();
-const multer = require('multer');
-const sharp = require('sharp');
 
-const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '.', 'uploads'),
-  filename: function (req, file, cb) {
-    cb(null, `IMG.jpg`);
-  },
-});
+router.use(fileUpload());
+router.post('/upload', async (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
 
-const upload = multer({
-  storage,
-});
-
-router.post('/upload', upload.single('file'), async (req, res) => {
-  res.json({
-    message: 'File Uploaded!',
+  const file = req.files.file;
+  file.mv(`${__dirname}/../client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({
+      fileName: file.name,
+      filePath: `/uploads/${file.name}`,
+    });
   });
 });
 
