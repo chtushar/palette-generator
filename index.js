586e7f294f16bd50fa69bbef3e9fe29fe2406d98
Tushar Choudhari <choudharit23@gmail.com>
2020-08-10 19:34:16 +0530

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const PORT = process.env.PORT;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(`${__dirname}/client/public`));
+  app.use(express.static(`${__dirname}/client/build`));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
