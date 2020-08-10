114c61e0c8d6dce3d2c9d947aa87da65753ce7c9
Tushar Choudhari <choudharit23@gmail.com>
2020-08-10 19:20:49 +0530

    package

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const imageRoute = require('./router/image');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 require('./db/mongoose');
 
 app.use(cors());
 app.use(express.json());
 app.use('/image', imageRoute);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${__dirname}/../client/public`));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
+  });
+}
 
 ///////////////////////////////////////////////
 
