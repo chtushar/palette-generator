2c8e73a2ab5d98aef08df9979011286f0abf9c11
Tushar Choudhari <choudharit23@gmail.com>
2020-08-10 19:29:46 +0530

    deploy error

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app');
 const PORT = process.env.PORT;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${__dirname}/../client/public`));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -9,13 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/image', imageRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(`${__dirname}/../client/public`));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'));
-  });
-}
-
 ///////////////////////////////////////////////
 
 module.exports = app;
